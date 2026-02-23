366fb7056482fc7a8dc060255c4e3139c1d72a99
jonayethossen <jonayethossen8921@gmail.com>
2026-02-23 14:55:28 +0600

    _

--- src/Components/MembersFeedBack.jsx
+++ src/Components/MembersFeedBack.jsx
@@ -29,6 +29,7 @@ const MemberFeedback = () => {
         </h2>
 
         {/* Feedback Cards Grid */}
+        {/* feedback */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {feedbacks.map((item) => (
             <div
